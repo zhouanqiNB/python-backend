30b9f006f33b45d649dda4f34cfdc9dff28bf85f
zhouanqi <angiechou1231@gmail.com>
2023-04-14 03:10:25 +0800

    优化

--- query_all.py
+++ query_all.py
@@ -21,26 +21,28 @@ def do_query_all_node(tx):
         node_id
         labels
     """
-    query = "MATCH (n) return n"
+    # 统计节点边的数量
     query2 = "match (n)-[]-() return ID(n) as id,count(*) as count"
-    records = tx.run(query)
     records2 = tx.run(query2)
-    id2EdgeCount = {}
+    id_2_edge_count = {}
     for record in records2:
-        id2EdgeCount[record.get("id")] = record.get("count")
+        id_2_edge_count[record.get("id")] = record.get("count")
+
+    # 节点基本信息
+    query = "MATCH (n) return ID(n) as id, LABELS(n) as labels"
+    records = tx.run(query)
     res = []
     for record in records:
-        node_id = int(record.get('n').element_id[39:])
-        labels = list(record.get('n').labels)
-        edgeCount = 1
-        if node_id in id2EdgeCount:
-            edgeCount = id2EdgeCount[node_id]
+        node_id = record.get('id')
+        labels = list(record.get('labels'))
+        edge_count = 1
+        if node_id in id_2_edge_count:
+            edge_count = id_2_edge_count[node_id]
         res.append({
             "node_id": node_id,
             "label": labels[0],
-            "value": edgeCount
+            "value": edge_count
         })
-    # print(res)
     return res
 
 
@@ -54,12 +56,12 @@ def do_query_all_relationship(tx):
         node_id_1
         node_id_2
     """
-    query = "MATCH (n1)-[r]->(n2) return n1,r,n2"
+    query = "MATCH (n1)-[r]->(n2) return ID(n1) as id_n1 ,ID(r) as id_r,ID(n2) as id_n2"
     res = []
     for record in tx.run(query):
-        relationship_id = int(record.get('r').element_id[39:])
-        node_id_1 = int(record.get('n1').element_id[39:])
-        node_id_2 = int(record.get('n2').element_id[39:])
+        relationship_id = record.get('id_r')
+        node_id_1 = record.get('id_n1')
+        node_id_2 = record.get('id_n2')
 
         res.append({
             "relationship_id": relationship_id,
@@ -67,5 +69,4 @@ def do_query_all_relationship(tx):
             "node_id_2": node_id_2
         })
 
-    # print(res)
     return res
